30ce5f50c78665759287f39be0ce74f8d4d5f4ed
Ahmedmayahi <80107996+AhmedMh0@users.noreply.github.com>
2023-08-10 18:32:56 +0300

    fix linters errors

--- src/modules/commentsCounter.test.js
+++ src/modules/commentsCounter.test.js
@@ -2,7 +2,7 @@ import { commentsCounter } from './commentsCounter.js';
 
 describe('test comments counter function', () => {
   test('calculate the number of comments for each meal item', async () => {
-    const mealId = "53061";
+    const mealId = '53061';
     const nbComments = await commentsCounter(mealId);
     expect(nbComments).toHaveLength(2);
   });
