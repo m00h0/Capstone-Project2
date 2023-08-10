4d2f4fe0b6de6161b7eb7231c87b6faf183e3f84
ajroudirida <hpwin10kamilio@gmail.com>
2023-08-10 15:12:56 +0100

    update the test

--- src/modules/commentsCounter.test.js
+++ src/modules/commentsCounter.test.js
@@ -2,8 +2,8 @@ import { commentsCounter } from './commentsCounter.js';
 
 describe('test comments counter function', () => {
   test('calculate the number of comments for each meal item', async () => {
-    const mealId = '52835';
+    const mealId = "53061";
     const nbComments = await commentsCounter(mealId);
-    expect(nbComments).toHaveLength(14);
+    expect(nbComments).toHaveLength(2);
   });
 });
